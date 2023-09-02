a30cb4b9aa10997efb9cd499f58f0e718d9eb950
Your Name <you@example.com>
2023-09-03 01:13:07 +0200

    Classes completed

--- Admin-Driver/src/app/app.module.ts
+++ Admin-Driver/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
 import { AdminHomeComponent } from './Components/admin-home/admin-home.component';
 import { DriverHomeComponent } from './Components/driver-home/driver-home.component';
+import { VehicleListComponentComponent } from './Components/vehicle-list-component/vehicle-list-component.component';
+import { BookingsListComponentComponent } from './Components/bookings-list-component/bookings-list-component.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { DriverListComponent } from './driver-list/driver-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     AdminHomeComponent,
-    DriverHomeComponent
+    DriverHomeComponent,
+    VehicleListComponentComponent,
+    BookingsListComponentComponent,
+    NavbarComponent,
+    DriverListComponent
   ],
   imports: [
     BrowserModule,
