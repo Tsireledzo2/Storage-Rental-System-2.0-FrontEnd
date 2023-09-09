05f2e95f5de15cce876c36bc9febf5ff28f03bef
DELL <220128871@mycput.ac.za>
2023-09-09 20:01:05 +0200

    220128871 Fetching data from the database

--- Customer/src/app/Components/booking/booking.component.ts
+++ Customer/src/app/Components/booking/booking.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageUnit } from 'src/app/models/storage-unit';
+import { StorageUnitService } from 'src/app/services/storage-unit.service';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.css']
 })
-export class BookingComponent {
+export class BookingComponent{
   quantity: number = 1;
   showExtraFields: boolean = false;
+  
 
   incrementQuantity() {
     if (this.quantity < 10) {
@@ -24,5 +27,9 @@ export class BookingComponent {
   toggleExtraFields(value: boolean) {
     this.showExtraFields = value;
   }
+
+  
+
+ 
   
 }
--- Customer/src/app/services/storage-unit.service.ts
+++ Customer/src/app/services/storage-unit.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { StorageUnit } from '../models/storage-unit';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +11,9 @@ export class StorageUnitService {
   private baseUrl = 'http://localhost:8080';
 
   constructor(private http: HttpClient) { }
+
+  fetchStorageUnit(storageId: string): Observable<StorageUnit> {
+    const url = `${this.baseUrl}/storageUnit/read/${storageId}`;
+    return this.http.get<StorageUnit>(url);
+  }
 }
